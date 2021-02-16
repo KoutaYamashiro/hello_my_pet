5e11aed129fbce0ee6012e8e20053474099acb00
kota.yamashiro <kota19890122@gmail.com>
2021-02-16 17:12:54 +0900

    Add fix FavoritesCreateServlet

--- src/controllers/favorites/FavoritesCreateServlet.java
+++ src/controllers/favorites/FavoritesCreateServlet.java
@@ -32,26 +32,42 @@ public class FavoritesCreateServlet extends HttpServlet {
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         // EntityManagerのオブジェクトを生成
         EntityManager em = DBUtil.createEntityManager();
 
+        // いいねを生成
         Favorite f = new Favorite();
 
         // いいねしたペットのIDを取得
-       Pet p = em.find(Pet.class, Integer.parseInt(request.getParameter("pet_id")));
+        Pet p = em.find(Pet.class, Integer.parseInt(request.getParameter("pet_id")));
 
         // テーブルに値をセット
-       f.setUser((User) request.getSession().getAttribute("login_user"));
-       f.setPet(p);
+        f.setUser((User) request.getSession().getAttribute("login_user"));
+        f.setPet(p);
 
-        // データベースを更新
-        em.getTransaction().begin();
-        em.persist(f);
-        em.getTransaction().commit();
-        em.close();
-        request.getSession().setAttribute("flush", "いいねしました。");
-        // トップページへリダイレクト
-        response.sendRedirect(request.getContextPath() + "/");
+        // すでにいいねしたペットの場合はエラーとなる
+//        List<String> error = FavoriteValidator.validate(f, true);
+//        if (error.size() > 0) {
+//            em.close();
+
+            request.setAttribute("_token", request.getSession().getId());
+            request.setAttribute("favorite", f);
+//            request.setAttribute("error", error);
+
+//            RequestDispatcher rd = request.getRequestDispatcher("/");
+//            rd.forward(request, response);
+//        } else {
+
+            // データベースを更新
+            em.getTransaction().begin();
+            em.persist(f);
+            em.getTransaction().commit();
+            em.close();
+            request.getSession().setAttribute("flush", "いいねしました。");
+            // トップページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/");
+        }
     }
-}
+//}
